78b02e87079a3b910cecd9d63ae77e2737f08185
minseomin <113421574+minseomin@users.noreply.github.com>
2022-10-11 17:46:14 +0900

    05

--- main.c
+++ main.c
@@ -4,18 +4,35 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-int square(int a)
-{
-	return a*a;
-}
 
-
-int main(int argc, char *argv[]) {
-	int a = 2;
-	
-	a= square(a);
-	
-	printf("a=%i\n",a);
+int main() {
+	int a, b;
+	a=get_integer();
+	b=get_integer();
+	printf("c(%d,%d)=%d\n",a,b,combination(a,b));
 	return 0;
-
 }
+
+int combination(int n, int r){
+	return(factorial(n)/(factorial(r)*factorial(n-r)));
+}
+
+int factorial(int n)
+{
+	int res = 1;
+	int i;
+	for(i=1; i<=n; i++)
+	{
+		res = res*i;
+	}
+	return res;
+}
+
+int get_integer (void){
+	int n;
+	printf("정수를 입력하시오 :");
+	scanf("%d", &n);
+	return n;
+}
+
+
